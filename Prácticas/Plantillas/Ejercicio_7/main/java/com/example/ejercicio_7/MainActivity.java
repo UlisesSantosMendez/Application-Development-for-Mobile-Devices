0ce55c335fc98c76ddf9f12836945af1ec3467d9
Ulises Santos Mendez <96894230+UlisesSantosMendez@users.noreply.github.com>
2023-07-03 21:03:23 -0600

    Add files via upload

---
+++ Prácticas/Plantillas/Ejercicio_7/main/java/com/example/ejercicio_7/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ejercicio_7;
+
+import android.os.*;
+import android.app.*;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.activity_main);
+    }
+}
