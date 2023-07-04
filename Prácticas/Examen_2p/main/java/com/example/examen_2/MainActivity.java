22c53ee8974fbc4d0f96d6c6195e9f61118928e1
Ulises Santos Mendez <96894230+UlisesSantosMendez@users.noreply.github.com>
2023-07-03 20:48:59 -0600

    Add files via upload

---
+++ Prácticas/Examen_2p/main/java/com/example/examen_2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.examen_2;
+
+import android.app.*;
+
+import android.os.*;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.activity_main);
+    }
+}
