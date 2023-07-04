c9a9287bb370f3132c8e887a7279fac854e77976
Ulises Santos Mendez <96894230+UlisesSantosMendez@users.noreply.github.com>
2023-07-04 07:04:41 -0600

    Add files via upload

---
+++ Prácticas/Tarea-Listas/ejemplo2/main/java/com/example/listas2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.listas2;
+
+import android.app.*;
+import android.os.*;
+import java.util.*;
+import android.widget.*;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.activity_main);
+        final ListView lv = (ListView) findViewById(R.id.xlv1);
+        final NuevaEntradaAdapter nea = new NuevaEntradaAdapter(this, R.layout.nueva_entrada_lista);
+        lv.setAdapter(nea);
+        for(final NuevaEntrada i : getEntradas()) {
+            nea.add(i);
+        }
+    }
+    private List<NuevaEntrada> getEntradas() {
+        final List<NuevaEntrada> datos = new ArrayList<NuevaEntrada>();
+        for (int i = 1; i < 31; i++) {
+            datos.add(new NuevaEntrada("Datos de Entrada" + i, "Ulises ESCOM" + i, new GregorianCalendar(2016,12,i).getTime(),i % 2 == 0 ? R.drawable.icon_1 : R.drawable.icon_2));
+        }
+        return datos;
+    }
+}
