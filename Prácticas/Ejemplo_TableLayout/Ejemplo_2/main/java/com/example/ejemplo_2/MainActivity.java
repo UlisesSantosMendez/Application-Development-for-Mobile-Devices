a8b4ced8ec260a791e97cb66599eb339be366042
Ulises Santos Mendez <96894230+UlisesSantosMendez@users.noreply.github.com>
2023-07-03 20:42:02 -0600

    Add files via upload

---
+++ Prácticas/Ejemplo_TableLayout/Ejemplo_2/main/java/com/example/ejemplo_2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ejemplo_2;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.activity_main);
+    }
+}
